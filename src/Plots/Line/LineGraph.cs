0bfd876016538330acdb274502ca389d00427ff6
Mike Gutmann <mikeg@fringebits.com>
2019-07-17 07:58:11 -0700

    Added ObservableColletion to LineGraph for binding.

--- src/Plots/Line/LineGraph.cs
+++ src/Plots/Line/LineGraph.cs
@@ -13,13 +13,16 @@ using System.ComponentModel;
 
 namespace InteractiveDataDisplay.WPF
 {
+    using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
+
     /// <summary>
     /// A plot to draw simple line.
     /// </summary>
     [Description("Plots line graph")]
     public class LineGraph : Plot
     {
-        private Polyline polyline;
+        protected Polyline polyline;
 
         /// <summary>
         /// Gets or sets line graph points.
@@ -57,10 +60,54 @@ namespace InteractiveDataDisplay.WPF
 
             Children.Add(polyline);
         }
+
         static LineGraph()
         {
             PointsProperty.OverrideMetadata(typeof(LineGraph), new PropertyMetadata(new PointCollection(), PointsPropertyChangedHandler) );
         }
+
+        public ObservableCollection<Point> ObservablePoints
+        {
+            get { return (ObservableCollection<Point>)GetValue(ObservablePointsProperty); }
+
+            set { SetValue(ObservablePointsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ObservablePointsProperty =
+            DependencyProperty.RegisterAttached(
+                "ObservablePoints",
+                typeof(ObservableCollection<Point>),
+                typeof(LineGraph),
+                new PropertyMetadata(
+                    new ObservableCollection<Point>(),
+                    (d, e) =>
+                    {
+                        var linePlot = (LineGraph)d;
+                        var updateAction = new NotifyCollectionChangedEventHandler(
+                            (o, args) =>
+                            {
+                                if (linePlot != null)
+                                {
+                                    InteractiveDataDisplay.WPF.Plot.SetPoints(linePlot.polyline, new PointCollection((ObservableCollection<Point>)o));
+                                }
+                            });
+
+                        if (e.OldValue != null)
+                        {
+                            var coll = (INotifyCollectionChanged)e.OldValue;
+                            coll.CollectionChanged -= updateAction;
+                        }
+
+                        if (e.NewValue != null)
+                        {
+                            var coll = (INotifyCollectionChanged)e.NewValue;
+                            coll.CollectionChanged += updateAction;
+                            if (linePlot != null)
+                            {
+                                InteractiveDataDisplay.WPF.Plot.SetPoints(linePlot.polyline, new PointCollection((ObservableCollection<Point>)e.NewValue));
+                            }
+                        }
+                    }));
 
         /// <summary>
         /// Updates data in <see cref="Points"/> and causes a redrawing of line graph.
