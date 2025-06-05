24fb83ebf25a44ca3472b9df952b1a8369005d2b
Mike Gutmann <mikeg@fringebits.com>
2025-06-05 08:49:59 -0700

    Project update.

--- src/Plots/BingMaps/BingMapsChart.cs
+++ src/Plots/BingMaps/BingMapsChart.cs
@@ -5,7 +5,7 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using Microsoft.Maps.MapControl.WPF;
+using Helix.MapControl;
 
 namespace InteractiveDataDisplay.WPF
 {
